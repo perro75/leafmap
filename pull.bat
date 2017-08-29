a6fe42c4cd050e0d04a5bd9f60722f25a7382a68
Engström Stefan <stefan.engstrom@liikennevirasto.fi>
2017-08-29 12:25:56 +0300

    Removed batches

--- pull.bat
+++
@@ -1,3 +0,0 @@
-..\..\bin\git.exe init
-..\..\bin\git.exe pull git://github.com/perro75/leafmap.git
-pause
--- push.bat
+++
@@ -1,5 +0,0 @@
-..\..\bin\git.exe add .
-..\..\bin\git.exe commit -m "Testing"
-
-..\..\bin\git.exe push --set-upstream https://github.com/perro75/leafmap.git master
-pause
